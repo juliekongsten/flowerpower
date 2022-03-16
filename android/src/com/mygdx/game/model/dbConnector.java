730b474fbefc1a0e19ca5998f864688b6537e416
julievk <julievk@stud.ntnu.no>
2022-03-16 17:22:31 +0100

    #3 implemented interfacing between android and core module

--- android/src/com/mygdx/game/model/dbConnector.java
+++ android/src/com/mygdx/game/model/dbConnector.java
@@ -1,6 +1,10 @@
 package com.mygdx.game.model;
 
+import androidx.annotation.NonNull;
+
 import com.badlogic.gdx.Gdx;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.mygdx.game.db;
@@ -8,10 +12,12 @@ import com.mygdx.game.db;
 public class dbConnector implements db {
      private FirebaseDatabase database;
      private DatabaseReference myRef;
+     private DatabaseReference newRef;
 
      public dbConnector() {
          database = FirebaseDatabase.getInstance("https://flowerpower-9b405-default-rtdb.europe-west1.firebasedatabase.app");
          myRef = database.getReference("message");
+         newRef = database.getReference("read");
      }
      //FirebaseDatabase database = FirebaseDatabase.getInstance("https://flowerpower-9b405-default-rtdb.europe-west1.firebasedatabase.app/");
      //DatabaseReference myRef = database.getReference("message");
@@ -23,4 +29,11 @@ public class dbConnector implements db {
         Gdx.app.log("android", message);
         myRef.setValue(message);
     }
-}
+
+    newRef.child("read").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+       @Override
+        public void onComplete(@NonNull Task<DataSnapshot> task) {
+              if (!task.isSuccessful()) {
+                          Log.e("firebase", "Error getting data", task.getException());
+                      } }     
+    }
