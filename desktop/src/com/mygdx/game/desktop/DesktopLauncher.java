ae44707dd97a3f8e123467db9bb30745a1a99d4d
Cecilie Knudsen <cecilikn@stud.ntnu.no>
2022-03-29 14:50:05 +0200

    version fix

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -1,25 +1,29 @@
 package com.mygdx.game.desktop;
-/*
+//NIGHTLY
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
-import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;*/
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 
-
+//LATEST
+/*
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+ */
 import com.mygdx.game.FlowerPowerGame;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		// LATEST:
-
+		/*
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		new LwjglApplication(new FlowerPowerGame(new fireBaseInterfaceDesktop()), config);
+		*/
+
 		// NIGHTLY:
-		/*
+
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		new Lwjgl3Application(new FlowerPowerGame(new fireBaseInterfaceDesktop()), config);
 
-		 */
 	}
 }
 
