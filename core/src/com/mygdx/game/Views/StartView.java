450e9933d81443c4fdc9c05b24fcfae938bdf798
natalikr <natalikr@stud.ntnu.no>
2022-03-30 12:44:51 +0200

    #32 Made register and login texture

--- core/src/com/mygdx/game/Views/StartView.java
+++ core/src/com/mygdx/game/Views/StartView.java
@@ -1,11 +1,18 @@
 package com.mygdx.game.Views;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
 public class StartView extends View{
     private Texture logo;
+    private Texture register;
+    private Texture login;
+
 
 
     public StartView(ViewManager vm) {
@@ -29,6 +36,7 @@ public class StartView extends View{
         ScreenUtils.clear((float)180/255,(float)245/255,(float) 162/255,1);
         sb.draw(logo,36,350);
         sb.end();
+
 
 
     }
