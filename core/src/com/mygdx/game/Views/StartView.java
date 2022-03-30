011bca67b8231d42fe46ada21b42aac885f70244
natalikr <natalikr@stud.ntnu.no>
2022-03-30 13:11:30 +0200

    #32 Ma

--- core/src/com/mygdx/game/Views/StartView.java
+++ core/src/com/mygdx/game/Views/StartView.java
@@ -3,6 +3,7 @@ package com.mygdx.game.Views;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.utils.ScreenUtils;
@@ -18,10 +19,16 @@ public class StartView extends View{
     public StartView(ViewManager vm) {
         super(vm);
         logo = new Texture("logo.png");
+        register = new Texture("register.png");
+        login = new Texture("login.png");
     }
 
     @Override
     protected void handleInput() {
+        if(Gdx.input.isTouched()){
+            Vector3 tmp = new Vector3(Gdx.input.getX(), Gdx.input.getY(),0);
+
+        }
 
     }
 
@@ -35,6 +42,8 @@ public class StartView extends View{
         sb.begin();
         ScreenUtils.clear((float)180/255,(float)245/255,(float) 162/255,1);
         sb.draw(logo,36,350);
+        sb.draw(register,100,175);
+        sb.draw(login,125,100);
         sb.end();
 
 
