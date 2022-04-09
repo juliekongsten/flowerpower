61c3fd723e5a0afb52da19d736ece8c302c119fc
sandrakvaloy <sandrakv@stud.ntnu.no>
2022-04-09 20:01:40 +0200

    #5 made message for waiting/your turn showing based on boolean

--- core/src/com/mygdx/game/Views/GameView.java
+++ core/src/com/mygdx/game/Views/GameView.java
@@ -7,10 +7,14 @@ import com.mygdx.game.FlowerPowerGame;
 
 public class GameView extends View{
 
+    private boolean waiting = true;
+
     private final Texture pool;
     private Texture ready;
     private Texture op_board;
     private Texture my_board;
+    private Texture my_turn;
+    private Texture op_turn;
 
 
 
@@ -20,6 +24,8 @@ public class GameView extends View{
         ready = new Texture("Button.png");
         op_board = new Texture("board.png");
         my_board = new Texture("board.png");
+        my_turn = new Texture("my_turn.png");
+        op_turn = new Texture("waiting.png");
 
     }
 
@@ -39,7 +45,9 @@ public class GameView extends View{
         sb.setProjectionMatrix(cam.combined);
         sb.begin();
         ScreenUtils.clear((float)254/255,(float)144/255,(float) 182/255,1);
-        //sb.draw(ready, 100, 50);
+
+
+        //her må vi ha noe opplegg at denne kun skal vises om man er i "plassere beds stadier"
         float ready_x = (float)(FlowerPowerGame.WIDTH/2-ready.getWidth()/2);
         sb.draw(ready,ready_x, -10);
 
@@ -53,6 +61,20 @@ public class GameView extends View{
 
         float op_board_y = pool_y+pool.getHeight()+2;
         sb.draw(op_board, board_x, op_board_y);
+
+        float my_turn_x = pool_x+(pool.getWidth()/2)-my_turn.getWidth()/2;
+        float my_turn_y = pool_y+(pool.getHeight()/2)-my_turn.getHeight()/2;
+
+        float waiting_x = pool_x+(pool.getWidth()/2-op_turn.getWidth()/2);
+        float waiting_y = pool_y+(pool.getHeight()/2)-op_turn.getHeight()/2;
+
+        if (!waiting){
+            sb.draw(my_turn, my_turn_x, my_turn_y);
+        }
+        else{
+            sb.draw(op_turn, waiting_x, waiting_y);
+        }
+
         sb.end();
 
     }
