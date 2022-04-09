8c3db666a5a6b22322d763fb76d33266c6dea31e
sandrakvaloy <sandrakv@stud.ntnu.no>
2022-04-09 19:22:26 +0200

    #5 started adding ready-button and pool, but having problems

--- core/src/com/mygdx/game/Views/GameView.java
+++ core/src/com/mygdx/game/Views/GameView.java
@@ -1,13 +1,20 @@
 package com.mygdx.game.Views;
 
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.mygdx.game.FlowerPowerGame;
 
 public class GameView extends View{
+
+    private final Texture pool;
+    private Texture ready;
 
 
     protected GameView(ViewManager vm) {
         super(vm);
+        pool = new Texture("pool.png");
+        ready = new Texture("Button.png");
     }
 
     @Override
@@ -25,6 +32,10 @@ public class GameView extends View{
 
         sb.setProjectionMatrix(cam.combined);
         sb.begin();
+        sb.draw(ready, 100, 50);
+        //sb.draw(ready,(float) (FlowerPowerGame.WIDTH/2-ready.getWidth()/2), (float) 100 );
+        //sb.draw(pool, (float) (FlowerPowerGame.WIDTH/2-pool.getWidth()/2),(float)(FlowerPowerGame.HEIGHT/2-pool.getHeight()/2));
+        sb.draw(pool, 10, 200);
         ScreenUtils.clear((float)254/255,(float)144/255,(float) 182/255,1);
         sb.end();
 
