47c0174f300607c16c48691cc840b235cb49e92d
sandrakvaloy <sandrakv@stud.ntnu.no>
2022-04-10 15:53:06 +0200

    #5 made gamecontroller to create boards

---
+++ core/src/com/mygdx/game/Controllers/GameController.java
@@ -0,0 +1,42 @@
+package com.mygdx.game.Controllers;
+
+import com.mygdx.game.Models.Square;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GameController {
+    private List<Square> opBoard = new ArrayList<>();
+    private List<Square> myBoard = new ArrayList<>();
+    private int squaresize;
+    private int numberSquaresHeight;
+    private int numberSquaresWidth;
+
+    public GameController(){
+        squaresize = 32;
+        numberSquaresHeight = 5;
+        numberSquaresWidth = 9;
+        int x = 20;
+        int my_y = 50;
+        int op_y = 300;
+        for (int i = 0; i< numberSquaresHeight; i++){
+            for (int j = 0; j< numberSquaresWidth; j++){
+                myBoard.add(new Square(x, my_y, squaresize));
+                opBoard.add(new Square(x,op_y,squaresize));
+                x+=squaresize;
+                my_y+=squaresize;
+                op_y+=squaresize;
+            }
+        }
+
+    }
+
+    public List<Square> getOpBoard(){
+        return opBoard;
+    }
+
+    public List<Square> getMyBoard(){
+        return myBoard;
+    }
+
+}
