b62f8917642c17012f83729c45420ce18bf83ddf
natalikr <natalikr@stud.ntnu.no>
2022-04-21 15:04:02 +0200

    #41 Continued working on HighscoreView

--- core/src/com/mygdx/game/View/HighscoreView.java
+++ core/src/com/mygdx/game/View/HighscoreView.java
@@ -1,25 +1,39 @@
 package com.mygdx.game.View;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.mygdx.game.FlowerPowerGame;
 
 public class HighscoreView extends View{
+
     private final Texture logo;
+    private final Texture back;
 
     protected HighscoreView(ViewManager vm) {
         super(vm);
         logo = new Texture("logo.png");
+        back = new Texture("back.png");
     }
 
     @Override
     protected void handleInput() {
+        if(Gdx.input.justTouched()) {
+            Vector3 pos = cam.unproject(new Vector3(Gdx.input.getX(), Gdx.input.getY(), 0));
+            Rectangle backBounds = new Rectangle(10, FlowerPowerGame.HEIGHT-20, back.getWidth(), back.getHeight());
+            if (backBounds.contains(pos.x, pos.y)) {
+                vm.set(new MenuView(vm));
+            }
+        }
 
     }
 
     @Override
     public void update(float dt) {
-
+        handleInput();
     }
 
     @Override
@@ -28,6 +42,7 @@ public class HighscoreView extends View{
         sb.begin();
         ScreenUtils.clear((float)180/255,(float)245/255,(float) 162/255,1);
         sb.draw(logo,36,375);
+        sb.draw(back, 10, FlowerPowerGame.HEIGHT-20);
         sb.end();
 
     }
