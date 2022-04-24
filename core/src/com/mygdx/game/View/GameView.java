3f5aac61b8e383570f610e4be7bf491a20dc83c4
natalikr <natalikr@stud.ntnu.no>
2022-04-24 10:52:26 +0200

    #42 added clear players in gamview

--- core/src/com/mygdx/game/View/GameView.java
+++ core/src/com/mygdx/game/View/GameView.java
@@ -174,6 +174,7 @@ public class GameView extends View{
                 if(yesBounds.contains(pos.x,pos.y)){
                     gameController.myForfeited();
                     vm.set(new ExitView(vm, false, this.gameController));
+                    gameController.clearPlayers();
                 }
             }
                    
@@ -181,6 +182,7 @@ public class GameView extends View{
                 Rectangle exit_gameBounds = new Rectangle(FlowerPowerGame.WIDTH/2-exit_game.getWidth()/2,FlowerPowerGame.HEIGHT/2-100,exit_game.getWidth(),exit_game.getHeight());
                 if(exit_gameBounds.contains(pos.x,pos.y)){
                     vm.set(new ExitView(vm,true, this.gameController));
+                    gameController.clearPlayers();
                 }
         }}
     }
