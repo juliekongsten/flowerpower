d4ba19def98fe9635eaac902a96ed079b3361264
emmatk <emmatk@stud.ntnu.no>
2022-04-24 13:41:41 +0200

    Update, should be working now in MenuView

--- core/src/com/mygdx/game/View/MenuView.java
+++ core/src/com/mygdx/game/View/MenuView.java
@@ -48,9 +48,18 @@ public class MenuView extends View {
         highscoreButton = highscore.getButton();
         setHighscoreButtonEvent();
         stage.addActor(highscoreButton);
+
+        checkGame();
     }
 
-    private void setPlaybookButtonEvent() {
+    private void checkGame() {
+        if (gameController.getGID() > 0) {
+            gameController.deleteGame();
+        }
+    }
+
+
+        private void setPlaybookButtonEvent() {
         playbookButton.addListener(new EventListener()
         {
             @Override
