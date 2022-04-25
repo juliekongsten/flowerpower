e3876dba875125d4eea8c5464680ed7ee05f066d
natalikr <natalikr@stud.ntnu.no>
2022-04-25 11:02:02 +0200

    #53 Added backbutton

--- core/src/com/mygdx/game/View/PlaybookView.java
+++ core/src/com/mygdx/game/View/PlaybookView.java
@@ -1,12 +1,44 @@
 package com.mygdx.game.View;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Event;
+import com.badlogic.gdx.scenes.scene2d.EventListener;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.mygdx.game.Controller.ButtonController;
+import com.mygdx.game.FlowerPowerGame;
 
 public class PlaybookView extends View{
 
-    protected PlaybookView(ViewManager vm) {
+    private final ImageButton backButton;
+    private ButtonController buttonController;
+    private final Stage stage;
+
+
+    public PlaybookView(ViewManager vm) {
         super(vm);
+        stage = new Stage(new FitViewport(FlowerPowerGame.WIDTH, FlowerPowerGame.HEIGHT));
+        Gdx.input.setInputProcessor(stage);
+        this.buttonController = new ButtonController();
+        backButton = buttonController.getBackButton();
+        setBackButtonEvent();
+        stage.addActor(backButton);
+    }
+
+    private void setBackButtonEvent() {
+        backButton.addListener(new EventListener()
+        {
+            @Override
+            public boolean handle(Event event)
+            {
+                //Handle the input event.
+                vm.set(new MenuView(vm));
+                return true;
+            }
+        });
     }
 
     @Override
